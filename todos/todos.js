8819079b3c53222d0e3adfb281ec47d402abc400
Bruno.Silva <brs.rsilva@gmail.com>
2020-07-22 23:25:08 -0300

    Finalizando aulas App Todos

--- todos/todos.js
+++ todos/todos.js
@@ -2,11 +2,10 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
-];
+
+// Recupera os dados do localStorage e converte para JSON ou caso não consiga, retornar um array vazio.
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
 
 function renderTodos(){
     listElement.innerHTML = '';
@@ -17,8 +16,12 @@ function renderTodos(){
 
         var linkElement = document.createElement('a');
         linkElement.setAttribute('href', "#");
-        var linkText = document.createTextNode('Excluir');
 
+        // indexOf -> retorna a posicao que esta o elemento "todo"
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
         linkElement.appendChild(linkText);
 
         todoElement.appendChild(todoText);
@@ -30,12 +33,28 @@ function renderTodos(){
 renderTodos();
 
 
+// Adicionar todo
 function addTodo(){
     var todoText = inputElement.value;
     todos.push(todoText);
     inputElement.value = '';
 
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
+
+
+// splice remove a "quantidade" passada após a "pos" passadas em parametro.
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+
+// Pega a lista de todo e salva no storage
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
